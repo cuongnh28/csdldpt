ef584663976a5ca61d0113c74b86e4e979ec73a7
hongcuongnguyen <hongcuongcl98@gmail.com>
2021-06-20 23:03:52 +0700

    final

--- save_model.py
+++ save_model.py
@@ -1,13 +1,14 @@
 from os import listdir
 from os.path import isfile, join
-from tika import parser
 titles = [f for f in listdir("data") if isfile(join("data", f))] #De lay titles la ten file.
 
 # luu cac doan van ban thanh mot list
 documents = []
+
 for file in titles:
-    raw = parser.from_file('data/' + file)
-    documents.append(raw['content'])
+    f = open('data/'+file, 'r')
+    documents.append(f.read())
+print(documents)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
@@ -26,16 +27,14 @@ class LemmaTokenizer:
         self.wnl = WordNetLemmatizer()
     def __call__(self, doc):
         return [self.wnl.lemmatize(t) for t in word_tokenize(doc) if t not in self.ignore_tokens]
-
 # Demonstrate the job of the tokenizer
 
 tokenizer=LemmaTokenizer()
 
 token_stop = tokenizer(' '.join(stop_words))
-vectorizer = TfidfVectorizer(stop_words=token_stop, tokenizer=tokenizer)
-
+vectorizer = TfidfVectorizer(stop_words=token_stop, tokenizer=tokenizer) #de luu tu vung va tokenize
 vectors = vectorizer.fit_transform(documents) # Trich xuat dac trung.
-
+# print(vectorizer.get_feature_names())
 import pickle
 
 with open('vectorizer.pkl', 'wb') as file:
